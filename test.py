b96eb2c29ecf23dda0adfe83e764a301801c5c56
damok79 <65806938+damok79@users.noreply.github.com>
2023-10-01 15:38:29 +0400

    Update test.py
    changing name and colour of text entry

--- test.py
+++ test.py
@@ -5,30 +5,37 @@ if 'chat_log' not in st.session_state:
     # If not, initialize it as an empty list
     st.session_state['chat_log'] = []
 
+# Check if 'user_input' is already in the session state
+if 'user_input' not in st.session_state:
+    # If not, initialize it as an empty string
+    st.session_state['user_input'] = ""
+
 def chatbot():
-    st.title("Simple Echo Chatbot")
+    st.title("Capybot v0.01")
 
     # Create placeholders for the chat log and the user input
     chat_placeholder = st.empty()
     input_placeholder = st.empty()
 
     # Get user input
-    user_input = input_placeholder.text_input("Enter your message")
+    st.session_state['user_input'] = input_placeholder.text_input("Enter your message", value=st.session_state['user_input'])
 
     if st.button("Send"):
         # Append user message and bot response to chat log
-        st.session_state.chat_log.append(f"You: {user_input}")
-        st.session_state.chat_log.append(f"Bot: {user_input}")
+        st.session_state.chat_log.append(f"You: {st.session_state['user_input']}")
+        st.session_state.chat_log.append(f"Bot: {st.session_state['user_input']}")
+
+        # Clear user input
+        st.session_state['user_input'] = ""
 
         # Display chat log
-        for message in st.session_state.chat_log:
-            chat_placeholder.write(message)
+        chat_placeholder.markdown("\n".join(st.session_state.chat_log))
 
     # Add custom CSS to change the color of the text entry box
     st.markdown("""
     <style>
     .stTextInput input {
-        background-color: #fafafa;
+        background-color: #d3d3d3;
     }
     </style>
     """, unsafe_allow_html=True)
